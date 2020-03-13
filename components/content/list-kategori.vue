1da3fc7f7bcd79a9a5729a6643ab7183eee279e7
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2020-03-13 17:19:07 +0700

    add create

---
+++ components/content/list-kategori.vue
@@ -0,0 +1,18 @@
+<template>
+  <select>
+    <option>{{ nama }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    nama: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style></style>
