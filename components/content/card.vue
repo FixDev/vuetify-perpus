f05eb85ad5bc539b4c7e01cba97c84fbf5e64755
MuchlasS <muchlas148@gmail.com>
2020-03-13 13:55:07 +0700

    fixing looping in category

--- components/content/card.vue
+++ components/content/card.vue
@@ -1,21 +1,48 @@
 <template>
     <div>
-        <v-container class="my-5">
-             <v-layout row wrap>
-                 <v-flex xs12 sm6 md4 lg3 v-for="category in categories" :key="category.judul">
-                     <v-card flat class="text-xs-center ma-3">
-                         <v-responsive class="pt-4">
-                             <v-img
-                                class="white--text align-end"
-                                height="200px"
-                                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                                >
-                                <v-card-title>{{ category.penulis}}</v-card-title>
-                            </v-img>
-                         </v-responsive>
-                     </v-card>
-                 </v-flex>
-             </v-layout>
+        <v-container fluid>
+            <v-row
+            :align="alignment"
+            :justify="justify">
+                <v-card
+                    v-for="kat in categories"
+                    :key="kat.judul"
+                    class="mx-auto"
+                    max-width="400"
+                >
+                    <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    :src="kat.img"
+                    >
+                    <v-card-title>{{kat.judul}}</v-card-title>
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">{{ kat.penulis }}</v-card-subtitle>
+
+                    <v-card-text class="text--primary">
+                    <div>Whitehaven Beach</div>
+
+                    <div>Whitsunday Island, Whitsunday Islands</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                    <v-btn
+                        color="orange"
+                        text
+                    >
+                        Share
+                    </v-btn>
+
+                    <v-btn
+                        color="orange"
+                        text
+                    >
+                        Explore
+                    </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-row>
         </v-container>
     </div>
 </template>
@@ -25,10 +52,10 @@
     data(){
         return{
             categories:[
-                { judul: 'Sejarah sejarah', penulis: 'Anton'},
-                { judul: 'Sejarah sejarah', penulis: 'Anton'},
-                { judul: 'Sejarah sejarah', penulis: 'Anton'},
-            ]
+                { judul: 'Sang Pangeran', penulis: 'Salim A Fillah', img: 'https://s1.bukalapak.com/img/10307414761/w-300/data.jpeg.webp', desc: 'Buku sejarah'},
+                { judul: 'Isa Bin Maryam', penulis: 'Ali Muhammad Ash Shallabi', img: 'https://s1.bukalapak.com/img/19684331581/w-300/data.jpeg.webp', desc: 'Sejarah Nabi Isa'},
+                { judul: 'Sejarah sejarah', penulis: 'Anton', img: 'https://s1.bukalapak.com/img/19684331581/w-300/data.jpeg.webp', desc: 'kosong'},
+            ],
         }
     }
   }
